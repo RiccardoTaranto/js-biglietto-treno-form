e77f4c468f7e860ff6140519d2ba9da35e50cd9d
Riccardo <riccardo.taranto@icloud.com>
2026-04-01 20:13:03 +0200

    add variables for km and age + console log

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 console.log('js biglietto treno');
 
-// MILESTONE 1
+// MILESTONE 1 - Scrivere un programma che chieda all’utente:
 
-// Scrivere un programma che chieda all’utente:
+// Il numero di chilometri da percorrere (input)
+const kmEl = document.getElementById('km')
 
-// Il numero di chilometri da percorrere,
+// Età del passeggero (input)
+const ageEl = document.getElementById('age')
 
-// Età del passeggero
-
-// usando esclusivamente due input e un bottone
+// bottone genera
+const generateBtnEl = document.getElementById('generateBtn')
+console.log(kmEl, ageEl, generateBtnEl);
 
 // La risposta finale (o output) da scrivere in console. 
+generateBtnEl.addEventListener('click', function(){
+
+    console.log('click');
+    console.log(kmEl.value);
+    console.log(ageEl.value);
+    
+})
+
+/*
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+*/
