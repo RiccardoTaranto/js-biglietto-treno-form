4e47504f2a9fe0b4574935588846f6d68b4e02fb
Riccardo <riccardo.taranto@icloud.com>
2026-04-01 18:14:30 +0200

    add milestone 1 comments and instructions in main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log('js biglietto treno');
+
+// MILESTONE 1
+
+// Scrivere un programma che chieda all’utente:
+
+// Il numero di chilometri da percorrere,
+
+// Età del passeggero
+
+// usando esclusivamente due input e un bottone
+
+// La risposta finale (o output) da scrivere in console. 
