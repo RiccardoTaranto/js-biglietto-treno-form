32debc3f3ec49ed7633207ee460c44a9c5612307
Riccardo <riccardo.taranto@icloud.com>
2026-04-02 10:08:26 +0200

    add ticket price calculation with age conditions  and event listener logic

--- main.js
+++ main.js
@@ -12,17 +12,25 @@ const ageEl = document.getElementById('age')
 const generateBtnEl = document.getElementById('generateBtn')
 console.log(kmEl, ageEl, generateBtnEl);
 
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+const rateKm = 0.21
+
 // La risposta finale (o output) da scrivere in console. 
-generateBtnEl.addEventListener('click', function(){
+generateBtnEl.addEventListener('click', function () {
+    const km = Number(kmEl.value)
+    const age = Number(ageEl.value)
+    // console.log(km, age);
 
-    console.log('click');
-    console.log(kmEl.value);
-    console.log(ageEl.value);
-    
+    let ticketRate = km * rateKm
+
+    if (age < 18) {
+        // applica sconto del 20% per i minorenni
+        ticketRate *= 0.8
+    } else if (age > 65) {
+        // applica sconto del 40% per gli over 65
+        ticketRate *= 0.6
+    }
+
+    console.log(ticketRate.toFixed(2));
+
 })
-
-/*
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-*/
